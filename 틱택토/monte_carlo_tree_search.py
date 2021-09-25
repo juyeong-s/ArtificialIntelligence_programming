a62332b06c44accc95d3fde0f10427226801e6ad
JuYeong <happyu9986@gmail.com>
2021-09-25 18:09:13 +0900

    [틱택토게임]MCTS.py - 주석추가

--- 틱택토/monte_carlo_tree_search.py
+++ 틱택토/monte_carlo_tree_search.py
@@ -1,7 +1,6 @@
-from abc import ABC, abstractmethod
-from collections import defaultdict
-import math
-
+from abc import ABC, abstractmethod # 추상 베이스 클래스
+from collections import defaultdict # 딕셔너리 생성 함수 임포트
+import math # math 모듈 임포트
 
 class MCTS:
     "Monte Carlo tree searcher. 먼저 rollout한 다음, 위치(move)선택"
@@ -14,64 +13,64 @@ class MCTS:
 
     def choose(self, node):
         "node의 최선인 자식 노드 선택"
-        if node.is_terminal(): # node가 단말인 경우 오류
-            raise RuntimeError(f"choose called on terminal node {node}")
+        if node.is_terminal(): # node가 단말인 경우
+            raise RuntimeError(f"choose called on terminal node {node}") # 런타임 에러
 
-        if node not in self.children: # node가 children에 포함되지 않으면 무작위 선택
-            return node.find_random_child()
+        if node not in self.children: # node가 children에 포함되지 않을 경우
+            return node.find_random_child() # 무작위로 선택한다.
 
         def score(n):  # 점수 계산
-            if self.N[n] == 0:
-                return float("-inf")  # 한번도 방문하지 않은 노드인 경우 - 선택 배제
-            return self.Q[n] / self.N[n]  # 평균 점수
+            if self.N[n] == 0: # 한번도 방문하지 않은 노드인 경우
+                return float("-inf") # 선택에서 제외
+            return self.Q[n] / self.N[n]  # 평균 점수 계산
 
-        return max(self.children[node], key=score)
+        return max(self.children[node], key=score) # children에 있는 node 중 최대 점수 리턴
 
     def do_rollout(self, node):
         "게임 트리에서 한 층만 더 보기"
-        path = self._select(node)
-        leaf = path[-1]
-        self._expand(leaf)
-        reward = self._simulate(leaf)
-        self._backpropagate(path, reward)
+        path = self._select(node) # 선택 단계로 진입
+        leaf = path[-1] # 자식노드
+        self._expand(leaf) # 자식노드 붙임
+        reward = self._simulate(leaf) # 자식노드를 시뮬레이션 단계로 보냄
+        self._backpropagate(path, reward) # 받아온 리워드로 역전파 단계로 보냄
 
     def _select(self, node): # 선택 단계
         "node의 아직 시도해보지 않은 자식 노드 찾기"
         path = []
-        while True:
-            path.append(node)
-            if node not in self.children or not self.children[node]:
-                # node의 child나 grandchild가 아닌 경우: 아직 시도해보지 않은 것 또는 단말 노드
+        while True: # 무한루프
+            path.append(node) # path에 해당 노드 추가
+            if node not in self.children or not self.children[node]: # node의 child나 grandchild가 아닌 경우
+                # 아직 시도해보지 않은 노드이거나 단말 노드임
                 return path
             unexplored = self.children[node] - self.children.keys() # 차집합
-            if unexplored:
-                n = unexplored.pop()
-                path.append(n)
-                return path
+            if unexplored: # 아직 시도해보지 않은 노드가 있을 경우
+                n = unexplored.pop() # 끝에걸 빼고
+                path.append(n) # path에 추가
+                return path # path 리턴
             node = self._uct_select(node)  # 한 단게 내려가기
 
     def _expand(self, node): # 확장 단계
         "children에 node의 자식노드 추가"
-        if node in self.children:
-            return  # 이미 확장된 노드
-        self.children[node] = node.find_children() # 선택가능 move들을 node의 children에 추가
+        if node in self.children: # children에 node가 있는 경우
+            return  # 이미 확장된 노드임
+        self.children[node] = node.find_children() # 선택가능한 move들을 node의 children에 추가
 
     def _simulate(self, node): # 시뮬레이션 단계
         "node의 무작위 시뮬레이션에 대한 결과(reward) 반환"
-        invert_reward = True
-        while True:
+        invert_reward = True # True로 초기화
+        while True: # 무한루프
             if node.is_terminal(): # 단말에 도달하면 승패여부 결정
-                reward = node.reward()
-                return 1 - reward if invert_reward else reward
+                reward = node.reward() # 리워드 반영
+                return 1 - reward if invert_reward else reward # invert_reward 가 True일 경우 1 - reward, False일 경우 1 - reward의 반대값 리턴
             node = node.find_random_child() # 선택할 수 있는 것 중에서 무작위로 선택
-            invert_reward = not invert_reward
+            invert_reward = not invert_reward # False로 변경
 
     def _backpropagate(self, path, reward): # 역전파 단계
         "단말 노드의 조상 노드들에게 보상(reward) 전달"
-        for node in reversed(path): # 역순으로 감ㄴ서 Monte Carlo 시뮬레이션 결과 반영
-            self.N[node] += 1
-            self.Q[node] += reward
-            reward = 1 - reward  # 자신에게는 1 상대에게는 0, 또는 그 반대
+        for node in reversed(path): # 역순으로 가면서 Monte Carlo 시뮬레이션 결과 반영
+            self.N[node] += 1 # 하나씩 올라감
+            self.Q[node] += reward # 리워드 반영
+            reward = 1 - reward  # 자신에게는 1 상대에게는 0이거나 그 반대
 
     def _uct_select(self, node): # UCB 정책 적용을 통한 노드 확장 대상 노드 선택
         "탐험(exploration)과 이용(exploitation)의 균형을 맞춰 node의 자식 노드 선택"
@@ -79,17 +78,17 @@ class MCTS:
         # node의 모든 자신 노드가 이미 확장되었는지 확인
         assert all(n in self.children for n in self.children[node])
 
-        log_N_vertex = math.log(self.N[node])
+        log_N_vertex = math.log(self.N[node]) # 부모노드 방문횟수 Log
 
         def uct(n):
             "UCB(Upper confidence bound) 점수 계산"
             return self.Q[n] / self.N[n] + self.exploration_weight * math.sqrt(
                 log_N_vertex / self.N[n]
-            )
+            ) # Q(v`) / N(v`) + c * sqrt(2 * LogN(v) / N(v`))
 
-        return max(self.children[node], key=uct)
+        return max(self.children[node], key=uct) ## UCB 가 큰 것선택
 
-
+## Node Class ##
 class Node(ABC):
     " 게임 트리의 노드로서 보드판의 상태 표현"
     @abstractmethod
