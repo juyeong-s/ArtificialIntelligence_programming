7a9a13431c8f2b51e0ce44a11960f93aae135786
JuYeong <happyu9986@gmail.com>
2021-10-08 16:48:31 +0900

    [AI]quadratic programming solver

--- Constrained optimization/constrained_optimization.py
+++ Constrained optimization/constrained_optimization.py
@@ -1,3 +1,12 @@
-import numpy as np
-import cvxopt
-from sklearn.datasets.samples_generator import make_
+from cvxopt import matrix, solvers
+
+H = 2*matrix([[1., 0.], [0., 1.]])
+f = matrix([0.0, 0.0])
+A = matrix([[0.0, 0.0], [0.0, -1.0]])
+a = matrix([0.0, -3/4.0])
+B = matrix([1.0, 1.0], (1, 2))
+b = matrix(1.0)
+
+sol = solvers.qp(H, f, A, a, B, b)
+print('헤\n', 'x_1 = ', sol['x'][0])
+print('x_2 = ', sol['x'][1])
