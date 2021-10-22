10a306f132facf6220e8df54d51ab01169c19eee
JuYeong <happyu9986@gmail.com>
2021-10-22 19:24:29 +0900

    [3-2][AI]결정트리-주석 추가

--- DicisionTree/decisionTree.py
+++ DicisionTree/decisionTree.py
@@ -1,11 +1,11 @@
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt # 그래프를 보여줄 패키지
+from sklearn.datasets import load_iris  # 데이터셋을 불러올 패키지
+from sklearn.tree import DecisionTreeClassifier, plot_tree  # 트리 생성을 위한 패키지
 
-iris = load_iris()
-decision_tree = DecisionTreeClassifier(criterion="entropy", random_state=0, max_depth=3)
-decision_tree = decision_tree.fit(iris.data, iris.target)
+iris = load_iris()  # 데이터 로드
+decision_tree = DecisionTreeClassifier(criterion="entropy", random_state=0, max_depth=3)    # 결정트리 모델 생성 : 트리 깊이 3까지 분류
+decision_tree = decision_tree.fit(iris.data, iris.target)   # DecisionTreeClassifier 학습하기
 
-plt.figure()
-plot_tree(decision_tree, filled=True)
-plt.show()
+plt.figure()    # 객체 생성
+plot_tree(decision_tree, filled=True)   # 결정 트리 
+plt.show()  # 창 열기
